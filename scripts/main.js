9fbd5cfaebe202a9979873190b4f5ad987e4f1f4
Arthur White <arthur@whitedevops.com>
2015-07-22 00:40:13 +0200

    A lot of new content

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,32 @@
-// Init highlight.js
-hljs.initHighlightingOnLoad();
+function hashInDocTableOfContents(hash) {
+	return $('.doc .table-of-contents ol a[href="'+hash+'"]').length > 0;
+}
+
+
+function loadDocContent(hash) {
+	$.get('/doc/'+hash.substr(1)+'.html', function(data) {
+		$('.doc .content').html(data);
+		$('pre code').each(function(i, block) {
+			hljs.highlightBlock(block);
+		});
+	});
+}
+
+
+$(document).ready(function() {
+
+	// Init highlight.js
+	hljs.initHighlighting();
+
+	// Load documentation
+	if (hashInDocTableOfContents(window.location.hash)) {
+		loadDocContent(window.location.hash);
+	} else {
+		loadDocContent($('.doc .table-of-contents ol a').attr('href'));
+	}
+
+	$('.doc .table-of-contents ol a').click(function() {
+		loadDocContent($(this).attr('href'));
+	});
+
+});
